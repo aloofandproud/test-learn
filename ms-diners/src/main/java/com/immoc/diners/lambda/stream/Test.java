a0d6c9de9c563f03c0b527e1d6bc7580859c3a98
OP-LWJ <784364520@qq.com>
2022-03-20 20:35:51 +0800

    00000

--- ms-diners/src/main/java/com/immoc/diners/lambda/stream/Test.java
+++ ms-diners/src/main/java/com/immoc/diners/lambda/stream/Test.java
@@ -4,4 +4,8 @@ public class Test {
     public static void main(String[] args) {
         System.out.println(1111);
     }
+    public static void xxx(){
+        System.out.println(1111111111);
+    }
+
 }
